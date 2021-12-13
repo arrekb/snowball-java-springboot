2f64427a201d31f006b98d43cd604da48bb83e4a
Arkadiusz Baraniecki <arkadiusz.baraniecki@allegro.pl>
2021-12-13 13:36:19 +0100

    fix

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ import com.google.cloud.bigquery.storage.v1.*;
 import com.google.protobuf.Descriptors;
 import org.json.JSONArray;
 import org.json.JSONObject;
- 
+
 import java.io.IOException;
 import java.time.Instant;
 
@@ -27,115 +27,112 @@ public class Application {
     static class WriteCommittedStream {
 
         final JsonStreamWriter jsonStreamWriter;
-    
+
         public WriteCommittedStream(String projectId, String datasetName, String tableName) throws IOException, Descriptors.DescriptorValidationException, InterruptedException {
-    
-          try (BigQueryWriteClient client = BigQueryWriteClient.create()) {
-    
-            WriteStream stream = WriteStream.newBuilder().setType(WriteStream.Type.COMMITTED).build();
-            TableName parentTable = TableName.of(projectId, datasetName, tableName);
-            CreateWriteStreamRequest createWriteStreamRequest =
+
+            try (BigQueryWriteClient client = BigQueryWriteClient.create()) {
+
+                WriteStream stream = WriteStream.newBuilder().setType(WriteStream.Type.COMMITTED).build();
+                TableName parentTable = TableName.of(projectId, datasetName, tableName);
+                CreateWriteStreamRequest createWriteStreamRequest =
                     CreateWriteStreamRequest.newBuilder()
-                            .setParent(parentTable.toString())
-                            .setWriteStream(stream)
-                            .build();
-    
-            WriteStream writeStream = client.createWriteStream(createWriteStreamRequest);
-    
-            jsonStreamWriter = JsonStreamWriter.newBuilder(writeStream.getName(), writeStream.getTableSchema()).build();
-          }
+                        .setParent(parentTable.toString())
+                        .setWriteStream(stream)
+                        .build();
+
+                WriteStream writeStream = client.createWriteStream(createWriteStreamRequest);
+
+                jsonStreamWriter = JsonStreamWriter.newBuilder(writeStream.getName(), writeStream.getTableSchema()).build();
+            }
         }
-    
+
         public ApiFuture<AppendRowsResponse> send(Arena arena) {
-          Instant now = Instant.now();
-          JSONArray jsonArray = new JSONArray();
-    
-          arena.state.forEach((url, playerState) -> {
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("x", playerState.x);
-            jsonObject.put("y", playerState.y);
-            jsonObject.put("direction", playerState.direction);
-            jsonObject.put("wasHit", playerState.wasHit);
-            jsonObject.put("score", playerState.score);
-            jsonObject.put("player", url);
-            jsonObject.put("timestamp", now.getEpochSecond() * 1000 * 1000);
-            jsonArray.put(jsonObject);
-          });
-    
-          return jsonStreamWriter.append(jsonArray);
+            Instant now = Instant.now();
+            JSONArray jsonArray = new JSONArray();
+
+            arena.state.forEach((url, playerState) -> {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("x", playerState.x);
+                jsonObject.put("y", playerState.y);
+                jsonObject.put("direction", playerState.direction);
+                jsonObject.put("wasHit", playerState.wasHit);
+                jsonObject.put("score", playerState.score);
+                jsonObject.put("player", url);
+                jsonObject.put("timestamp", now.getEpochSecond() * 1000 * 1000);
+                jsonArray.put(jsonObject);
+            });
+
+            return jsonStreamWriter.append(jsonArray);
         }
-    
-      }
-    
-      final String projectId = ServiceOptions.getDefaultProjectId();
-      final String datasetName = "snowball";
-      final String tableName = "events";
-    
-      final WriteCommittedStream writeCommittedStream;
-    
-      public Application() throws Descriptors.DescriptorValidationException, IOException, InterruptedException {
-        writeCommittedStream = new WriteCommittedStream(projectId, datasetName, tableName);
-      }
-    
 
-
-
-  static class Self {
-    public String href;
-  }
-
-  static class Links {
-    public Self self;
-  }
-
-  static class PlayerState {
-    public Integer x;
-    public Integer y;
-    public String direction;
-    public Boolean wasHit;
-    public Integer score;
-  }
-
-  static class Arena {
-    public List<Integer> dims;
-    public Map<String, PlayerState> state;
-  }
-
-  static class ArenaUpdate {
-    public Links _links;
-    public Arena arena;
-  }
-
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
-
-  @InitBinder
-  public void initBinder(WebDataBinder binder) {
-    binder.initDirectFieldAccess();
-  }
-
-  @GetMapping("/")
-  public String index() {
-    return "Let the battle begin!";
-  }
-
-  @PostMapping("/**")
-  public String index(@RequestBody ArenaUpdate arenaUpdate) {
-    System.out.println(arenaUpdate);
-    Random rand = new Random();
-    int n = rand.nextInt(10);
-
-    switch (n) {
-        case 0:
-            return "F";
-        case 1:
-            return "L";
-        case 2:
-            return "R";
     }
-    writeCommittedStream.send(arenaUpdate.arena);
-    return "T";
-  }
+
+    final String projectId = ServiceOptions.getDefaultProjectId();
+    final String datasetName = "snowball";
+    final String tableName = "events";
+
+    final WriteCommittedStream writeCommittedStream;
+
+    public Application() throws Descriptors.DescriptorValidationException, IOException, InterruptedException {
+        writeCommittedStream = new WriteCommittedStream(projectId, datasetName, tableName);
+    }
+
+
+    static class Self {
+        public String href;
+    }
+
+    static class Links {
+        public Self self;
+    }
+
+    static class PlayerState {
+        public Integer x;
+        public Integer y;
+        public String direction;
+        public Boolean wasHit;
+        public Integer score;
+    }
+
+    static class Arena {
+        public List<Integer> dims;
+        public Map<String, PlayerState> state;
+    }
+
+    static class ArenaUpdate {
+        public Links _links;
+        public Arena arena;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.initDirectFieldAccess();
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Let the battle begin!";
+    }
+
+    @PostMapping("/**")
+    public String index(@RequestBody ArenaUpdate arenaUpdate) {
+        System.out.println(arenaUpdate);
+        writeCommittedStream.send(arenaUpdate.arena);
+        Random rand = new Random();
+        int n = rand.nextInt(10);
+        switch (n) {
+            case 0:
+                return "F";
+            case 1:
+                return "L";
+            case 2:
+                return "R";
+        }
+        return "T";
+    }
 }
 
